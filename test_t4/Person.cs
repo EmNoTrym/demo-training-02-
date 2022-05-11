eaee2ea26366ce1fefc154d7531cbbcf42b2b70a
EmNoTrym <63888869+EmNoTrym@users.noreply.github.com>
2022-05-11 15:32:04 +0700

    Person class added

---
+++ test_t4/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_t4
+{
+    class Person
+    {
+    }
+}
--- test_t4/Program.cs
+++ test_t4/Program.cs
@@ -6,7 +6,7 @@ namespace test_t4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
