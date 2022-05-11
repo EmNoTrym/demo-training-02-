7a1dd5bdf005b71111e3e57283d57f09455de27c
EmNoTrym <63888869+EmNoTrym@users.noreply.github.com>
2022-05-11 16:07:17 +0700

    add 2 cs file

---
+++ test_t4/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_t4
+{
+    class Person
+    {
+        private string name { get; set; }
+        private int age { get; set; }
+
+        Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string older(Person a)
+        {
+            if (a.age > this.age)
+            {
+                return a.name;
+            }
+
+            else
+            {
+                return this.name;
+            }
+        }
+    }
+}
